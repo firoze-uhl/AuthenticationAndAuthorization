740564408004ef798fcaa2c7d764a75ff6d9ceed
Md. Firoze Hossain <firoze.hossain@uhlbd.com>
2024-07-07 12:11:08 +0600

    User Details Service Implementation done

---
+++ Auth/src/main/java/com/uhl/main/auth/security/config/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.uhl.main.auth.security.config;
+
+import com.uhl.main.auth.security.user.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
+
