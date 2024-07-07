901cd79ae085a7c7e605a492930a3ef17722ad01
Md. Firoze Hossain <firoze.hossain@uhlbd.com>
2024-07-07 12:39:54 +0600

    logic added for created two roles USER and ADMIN

--- Auth/src/main/java/com/uhl/main/auth/AuthApplication.java
+++ Auth/src/main/java/com/uhl/main/auth/AuthApplication.java
@@ -1,13 +1,33 @@
 package com.uhl.main.auth;
 
+import com.uhl.main.auth.security.user.Role;
+import com.uhl.main.auth.security.user.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner(RoleRepository roleRepository) {
+        return args -> {
+            Role userRole = new Role();
+            if (roleRepository.findByName("USER").isEmpty()) {
+                userRole.setName("USER");
+                roleRepository.save(userRole);
+            }
+            if (roleRepository.findByName("ADMIN").isEmpty()) {
+                Role adminRole = new Role();
+                adminRole.setName("ADMIN");
+                roleRepository.save(adminRole);
+            }
+        };
     }
 
 }
